15fee3aeabe4324c48139814d1f9eb60f9d628a0
raygong <ray>
2017-05-07 11:36:34 +0800

    update

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,6 +1,10 @@
 /**
- * Created by chenzhengguo on 2015/7/30.
- * Updated by chenzhengguo on 2016/1/28.
+ *
+ * # mockJS
+ ### 默认访问地址为 http://127.0.0.1:2618/main/xxx
+ ### 修改api.json 里面的`url`来自定义你的接口
+ 
+ `main`可以被更改为其他任何你想要的字符，请更改`server.js`里面的`prefix`
  */
 var path = require('path');
 var fs = require('fs');
